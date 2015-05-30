5e350f03249990b8b5958b91543949b55dc13f54
GryfonN <petranikpeter@gmail.com>
2015-05-30 21:40:45 +0200

    JSHINT spraveny

--- example-agency-project/gulpfile.js
+++ example-agency-project/gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
 
+var jshint = require('gulp-jshint');
+var size = require('gulp-size');
+
+
 gulp.task('hello-world', function () {
     console.log('Hello world');
+});
+
+gulp.task('jshint', function () {
+    gulp.src(['./src/**/*.js', './*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(size());
 });
